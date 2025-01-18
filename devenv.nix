4a899ced26ef333c4156ca4c85d9eaeac3ae1e13
Thales Menato <8753631+thamenato@users.noreply.github.com>
2025-01-18 17:19:55 -0500

    feat(mkdocs): add initial config

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,15 @@
 {
   packages = [ pkgs.git ];
 
-  languages.python.enable = true;
+  languages.python = {
+    enable = true;
+    venv = {
+      enable = true;
+      requirements = ''
+        mkdocs
+      '';
+    };
+  };
 
   # pre-commit.hooks.shellcheck.enable = true;
 }
